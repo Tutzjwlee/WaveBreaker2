cfa1105be96e4f3a6530935a2a564b6b50cd416b
Tutzjwlee <138728172+Tutzjwlee@users.noreply.github.com>
2024-10-29 18:48:36 -0300

    sadasd

---
+++ Assets/_Scripts/DataSO/WeaponDataSO.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapons/WeaponData")]
+public class WeaponDataSO : ScriptableObject
+{
+    public int AmmoCapacity { get; internal set; }
+    public bool AutomaticFire { get; internal set; }
+    public float WeaponDelay { get; internal set; }
+
+    internal int GetBulletCountToSpawn()
+    {
+        throw new NotImplementedException();
+    }
+}
